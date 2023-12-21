32969bddf657ed4161530b389cd2e7bdc93a4c92
anurag3811 <38anurag38@gmail.com>
2023-12-22 00:22:34 +0530

    responsive

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <div className='flex justify-center pt-8'>                                              {/* Nav */}
-      <div className='flex items-center'>                                                     {/* Image + Hypercubes.in */}
+      <div className='flex items-center md:flex-row flex-col'>                                                     {/* Image + Hypercubes.in */}
       <div className=' inline-flex'><Image src={logo} width={72} height={72}></Image></div>   {/* Image */}
       <div className='text-3xl lg:text-5xl inline-flex font-bold ml-2'>Hypercubes.in</div>    {/* Hypercubes.in */}
       </div>
