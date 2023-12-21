d17b25129c0e2b734ff5684c0ffc4a82d7a3a4c4
anurag3811 <38anurag38@gmail.com>
2023-12-22 00:25:25 +0530

    small up down

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
       <div className='flex justify-center pt-8'>                                              {/* Nav */}
       <div className='flex items-center md:flex-row flex-col'>                                                     {/* Image + Hypercubes.in */}
       <div className=' inline-flex'><Image src={logo} width={72} height={72}></Image></div>   {/* Image */}
-      <div className='text-3xl lg:text-5xl inline-flex font-bold ml-2'>Hypercubes.in</div>    {/* Hypercubes.in */}
+      <div className='text-3xl lg:text-5xl inline-flex font-bold ml-2 pt-2 md:pt-0'>Hypercubes.in</div>    {/* Hypercubes.in */}
       </div>
       </div>
       <div className='flex justify-center'><div className=' py-4'>Coming soon</div></div>
