65d6eb16d4afb4c6cd53f719063bfa24a4a11997
anurag3811 <38anurag38@gmail.com>
2023-12-22 00:13:14 +0530

    Final logo

--- app/page.js
+++ app/page.js
@@ -1,16 +1,15 @@
 import Image from 'next/image'
-import h from '../public/Hypercubes.png'
-
-
+import logo from '../public/logo.png'
 export default function Home() {
   return (
-    <main className='flex flex-col items-center justify-center'>
-      <div className='flex flex-col lg:flex-row items-center justify-center pt-16'>
-      <div className=' inline-flex'>
-      <Image src={h} width={200} height={200}></Image></div>
-      <div className='text-5xl lg:text-7xl inline-flex font-bold'>Hypercubes.in</div>
+    <>
+      <div className='flex justify-center pt-8'>                                              {/* Nav */}
+      <div className='flex items-center'>                                                     {/* Image + Hypercubes.in */}
+      <div className=' inline-flex'><Image src={logo} width={72} height={72}></Image></div>   {/* Image */}
+      <div className='text-3xl lg:text-5xl inline-flex font-bold ml-2'>Hypercubes.in</div>    {/* Hypercubes.in */}
       </div>
-      <div className=' py-4'>_ _ _ Coming soon _ _ _</div>
-    </main>
+      </div>
+      <div className='flex justify-center'><div className=' py-4'>Coming soon</div></div>
+    </>
   )
 }
